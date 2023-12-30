9dd9e203510b5b83ee4887c6e2c2e7d378a4cef0
Xiang Yu Tuang <xiangyutuang@gmail.com>
2023-12-29 22:51:28 -0500

    Wrote minimax algorithm for ai

--- main.py
+++ main.py
@@ -1,80 +1,125 @@
 import chess
+import math
 from tables import pawnscore, knightscore, bishopscore, queenscore, rookscore, kingscore_middle, kingscore_end
 board = chess.Board()
+
+#minmax algorithm
+def minimax(currMove, depth, maximizingPlayer):
+    if (depth == 0 or board.is_checkmate() or board.is_insufficient_material()):
+        return (material_eval(), currMove)
+    if maximizingPlayer:
+        maxEvaluation = -math.inf
+        bestMove = None
+        for move in board.legal_moves:
+            board.push(move)
+            evaluation,_ = minimax(move, depth - 1, not maximizingPlayer)
+            if (evaluation > maxEvaluation):
+                maxEvaluation = evaluation
+                bestMove = move
+            board.pop()
+        return (maxEvaluation, bestMove)
+    else:
+        minEvaluation = math.inf
+        bestMove = None
+        for move in board.legal_moves:
+            board.push(move)
+            evaluation,_ = minimax(move, depth - 1, not maximizingPlayer)
+            if (evaluation < minEvaluation):
+                minEvaluation = evaluation
+                bestMove = move
+            board.pop()
+        return (minEvaluation, bestMove)
 
 
 #simplified evaluation function from https://www.chessprogramming.org/Simplified_Evaluation_Function
 def material_eval():
-    currColor = board.turn
+    whitescore = 0
+    blackscore = 0
+    #check for checkmate
+    if (board.is_checkmate()):
+        if(board.turn == chess.BLACK):
+            whitescore = 9999
+        else:
+            blackscore = -9999
+        return whitescore if not board.turn else -blackscore
+    
+    if (board.is_stalemate() or board.is_insufficient_material()):
+        return 0
+
     #I really want to find a method that returns all the piece types
     #in an array and iterate through it, but I can't find in the python-chess docs
-    pawn_score = 0
-    knight_score = 0
-    bishop_score = 0
-    rook_score = 0
-    queen_score = 0
-    king_score = 0
+    numberOfWhiteQueens = len(board.pieces(chess.QUEEN, chess.WHITE))
+    numberOfBlackQueens = len(board.pieces(chess.QUEEN, chess.BLACK))
 
-    pawn_squares = board.pieces(chess.PAWN, currColor)
-    knight_squares = board.pieces(chess.KNIGHT, currColor)
-    bishop_squares = board.pieces(chess.BISHOP, currColor)
-    queen_squares = board.pieces(chess.QUEEN, currColor)
-    rook_squares = board.pieces(chess.ROOK, currColor)
-    king_squares = board.pieces(chess.KING, currColor)
+    pawn_squares = board.pieces(chess.PAWN, chess.WHITE)
+    knight_squares = board.pieces(chess.KNIGHT, chess.WHITE)
+    bishop_squares = board.pieces(chess.BISHOP, chess.WHITE)
+    rook_squares = board.pieces(chess.ROOK, chess.WHITE)
+    queen_squares = board.pieces(chess.QUEEN, chess.WHITE)
+    king_squares = board.pieces(chess.KING, chess.WHITE)
 
-    if (currColor):
-        
-        for square in pawn_squares:
-            pawn_score += pawnscore[square]
-
-        
-        for square in knight_squares:
-            knight_score += knightscore[square]
-        
-        
-        for square in bishop_squares:
-            bishop_score += bishopscore[square]
-        
-       
-        for square in  queen_squares:
-            queen_score += queenscore[square]
-
-        
-        for square in rook_squares:
-            rook_score += rookscore[square]
-
-        
-        for square in king_squares:
-            king_score += kingscore_middle[square]
-    else:
-        for square in pawn_squares:
-            pawn_score += pawnscore[chess.square_mirror(square)]
-
-        for square in knight_squares:
-            knight_score += knightscore[chess.square_mirror(square)]
-        
-        for square in bishop_squares:
-            bishop_score += bishopscore[chess.square_mirror(square)]
-        
-        for square in  queen_squares:
-            queen_score += queenscore[chess.square_mirror(square)]
-        
-        for square in rook_squares:
-            rook_score += rookscore[chess.square_mirror(square)]
-
-        for square in king_squares:
-            king_score += kingscore_middle[chess.square_mirror(square)]
-
-    return pawn_score + knight_score + bishop_score + rook_score + queen_score + king_score    
     
+    for square in pawn_squares:
+        whitescore += pawnscore[square]
+
+    for square in knight_squares:
+        whitescore += knightscore[square]
+
+    for square in bishop_squares:
+        whitescore += bishopscore[square]
+
+    for square in  queen_squares:
+        whitescore += queenscore[square]
+
+    for square in rook_squares:
+        whitescore += rookscore[square]
+
+    for square in king_squares:
+        if (numberOfWhiteQueens == 0 and numberOfBlackQueens == 0):
+            whitescore += kingscore_end[square]
+        else:
+            whitescore += kingscore_middle[square]
+
+    pawn_squares = board.pieces(chess.PAWN, chess.BLACK)
+    knight_squares = board.pieces(chess.KNIGHT, chess.BLACK)
+    bishop_squares = board.pieces(chess.BISHOP, chess.BLACK)
+    rook_squares = board.pieces(chess.ROOK, chess.BLACK)
+    queen_squares = board.pieces(chess.QUEEN, chess.BLACK)
+    king_squares = board.pieces(chess.KING, chess.BLACK)
+
+    for square in pawn_squares:
+        blackscore += pawnscore[chess.square_mirror(square)]
+
+    for square in knight_squares:
+        blackscore += knightscore[chess.square_mirror(square)]
+
+    for square in bishop_squares:
+        blackscore += bishopscore[chess.square_mirror(square)]
+        
+    for square in  queen_squares:
+        blackscore += queenscore[chess.square_mirror(square)]
+        
+    for square in rook_squares:
+        blackscore += rookscore[chess.square_mirror(square)]
+
+    for square in king_squares:
+        if (numberOfWhiteQueens == 0 and numberOfBlackQueens == 0):
+            blackscore += kingscore_end[chess.square_mirror(square)]
+        else:
+            blackscore += kingscore_middle[chess.square_mirror(square)]
+
+    return whitescore if not board.turn else -blackscore
+
 
 #game playing loop
 while (not board.is_checkmate()):
     print(board)
-    print(board.legal_moves)
     move = input('Please enter your move: ')
     board.push_san(move)
-    print(f'{"Black" if board.turn else "White"}={material_eval()}')
+    evaluated,aiMove = minimax("e7e5", 2, False)
+    print(f'AI chose {aiMove}')
+    board.push(aiMove)
+    print(material_eval())
 
 
 
--- readME.md
+++ readME.md
@@ -3,3 +3,10 @@ A chess game that works in the terminal and (hopefully) a working AI.
 Day 1:
 Wrote function to evaluate material. The calculation for material is based on
 the tables in tables.py. Also referenced https://www.chessprogramming.org/Simplified_Evaluation_Function.
+
+Day 2:
+Wrote minimax algorithm based on https://www.youtube.com/watch?v=l-hh51ncgDI. The minimax algorithm
+becomes extremely slow if you increase depth beyond 3. I'm pretty sure it's an issue of depth, that's
+why the AI makes some very weird moves.
+
+To-do: Add alpha-beta pruning and move validation for human users tomorrow.
